f03b344851cf18ff889841df0e986a1b31b7da7d
Tudor Bejinari <tudorbejinari@gmail.com>
2019-03-22 14:58:31 -0600

    adding files

---
+++ app/data/friends.js
@@ -0,0 +1,31 @@
+var friends = [
+  {
+    name: "Harps Joseph",
+    photo:
+      "https://images.unsplash.com/photo-1530268729831-4b0b9e170218?ixlib=rb-1.2.1&auto=format&fit=crop&w=1350&q=80",
+    scores: ["5", "2", "2", "5", "5", "1", "1", "5", "5", "4"]
+  },
+  {
+    name: "Sara Brown ",
+    photo:
+      "https://images.unsplash.com/photo-1531618090199-07f2f6613447?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1350&q=80",
+    scores: ["5", "5", "5", "5", "5", "1", "5", "1", "5", "1"]
+  },
+  {
+    name: "Ayo Ogunseinde ",
+    photo:
+      "https://images.unsplash.com/photo-1463453091185-61582044d556?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1350&q=80",
+    scores: ["5", "1", "1", "5", "5", "5", "5", "1", "3", "1"]
+  },
+  {
+    name: "Colleen Ryan",
+    photo: "https://images.unsplash.com/photo-1489424731084-a5d8b219a5bb?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=634&q=80",
+    scores: ["5", "5", "5", "5", "5", "5", "5", "1", "4", "3"]
+  },
+  {
+    name: "Alex Rodriguez",
+    photo: "https://images.unsplash.com/photo-1482264851290-446b18e3ee9f?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1350&q=80",
+    scores: ["2", "2", "5", "5", "4", "2", "2", "4", "2", "5"]
+  }
+];
+module.exports = friends;
